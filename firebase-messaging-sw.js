feb731d6e4947310a8865c10f56b93d69581935a
Alex Smith <alex.smith2844@gmail.com>
2020-12-24 17:23:03 +0500

    push

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,17 @@
-self.addEventListener("push", event => { event.waitUntil(onPush(event)) });
-self.addEventListener("notificationclick", event => { event.waitUntil(onNotificationClick(event)) });
+self.addEventListener("push", event => event.waitUntil(onPush(event)));
+self.addEventListener("notificationclick", event => event.waitUntil(onNotificationClick(event)));
 
 async function onPush(event) {
 	try {
 		let json = event.data.json();
 		console.log('push json received: ', json)
-		self.registration.showNotification(json.notification.title, {
-			body: json.notification.body,
-			icon: json.notification.icon,
-			sound: json.notification.sound
-		});
+		if ('data' in json) {
+			if ('gcm.notification.actions' in json.data)
+				json.notification.actions = JSON.parse(json.data['gcm.notification.actions']);
+			if ('gcm.notification.data' in json.data)
+				json.notification.data = json.data['gcm.notification.data'];
+		}
+		self.registration.showNotification(json.notification.title, json.notification);
 	} catch (e) {
 		let text = event.data.text();
 		console.error(e);
@@ -19,4 +21,9 @@ async function onPush(event) {
 }
 async function onNotificationClick(event) {
 	console.log('push click: ', event);
+	event.notification.close();
+	let query = new URLSearchParams(event.notification.data || '');
+	if (event.action)
+		query.append('action', event.action);
+	clients.openWindow((event.target.location.origin+'/')+'?'+query.toString());
 }
